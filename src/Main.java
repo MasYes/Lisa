f4083927e31f751d911927d16c61fcd23195355b
Julian <masyes@mail.com>
2013-07-17 18:06:31 +0400

    -First test

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args){
 		System.out.println(lisa.ExtractText.parse("D:\\example.pdf"));
+		System.out.println(lisa.ExtractText.parse("Something new"));
 	}
 }
 
